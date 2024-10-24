22bed526d4792ca6d5eec2a28c32ebbd9f98fef4
X3VI <kaylebarrels@gmail.com>
2024-10-25 01:59:10 +0200

    fixed ts lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -162,7 +162,7 @@ return {
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {},
+      ts_ls = {},
       ruff = {},
       pylsp = {
         settings = {
